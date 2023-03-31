38a766d4dd911648db572ab812ffb8642ff4627c
nazeekesen016 <60453904+nazeekesen016@users.noreply.github.com>
2023-03-31 17:58:27 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 4.61.0"
+   }
+ }
 }
+
 provider "aws" {
-  region = "us-east-1"
+ region = "us-east-1"
 }
 
 data "aws_availability_zones" "available" {}
